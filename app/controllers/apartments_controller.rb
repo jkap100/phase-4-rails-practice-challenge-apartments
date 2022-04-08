41b6288ee29af00c14f225484a57bf171c915016
Jon Kaplon <jon.kaplon1@gmail.com>
2022-04-08 09:18:07 -0700

    commit tree 1832cd41a7ac910931de7e3800834e744b9d5bec

---
+++ app/controllers/apartments_controller.rb
@@ -0,0 +1,37 @@
+class ApartmentsController < ApplicationController
+
+    before_action :find_apartment, except: [:index, :create]
+
+    def index
+        render json: Apartment.all
+    end
+
+    def show
+        render json: @apartment
+    end
+
+    def create
+        apartment = Apartment.create!(apartment_params)
+        render json: apartment, status: :created
+    end
+
+    def update
+        @apartment.update!(apartment_params)
+        render json: @apartment, status: :created
+    end
+
+    def destroy
+        @apartment.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_apartment
+        @apartment = Apartment.find(params[:id])
+    end
+
+    def apartment_params
+        params.permit(:number)
+    end
+end
